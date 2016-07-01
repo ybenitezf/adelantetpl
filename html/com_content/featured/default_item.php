00b8a96f4b1649171d7ca602d8e57c659da240af
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2016-07-01 16:00:59 -0400

    arregla imagen de menos destacados
    Las imagenes para los articulos menos destacados ya no daran
    problemas porque sin importar la alineación que se coja de la
    misma se mostraran centradas.

--- html/com_content/featured/default_item.php
+++ html/com_content/featured/default_item.php
@@ -58,11 +58,14 @@ $info    = $this->item->params->get('info_block_position', 0);
 
 <?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
 	<?php $imgfloat = (empty($images->float_intro)) ? $params->get('float_intro') : $images->float_intro; ?>
-	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
-	<?php if ($images->image_intro_caption):
-		echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
-	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/> </div>
+	<!-- <div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> -->
+	<div class="item-image">
+		<img
+		<?php if ($images->image_intro_caption):
+			echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
+		endif; ?>
+		src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/>
+ </div>
 <?php endif; ?>
 
 <?php if (!$params->get('show_intro')) : ?>
